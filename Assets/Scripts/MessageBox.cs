7dc2934f70c3cf7939c66c3446487d49dea18dcb
Pedro Nascimento <pedrovitor.marques@gmail.com>
2019-12-11 18:09:36 -0300

    Sistema básico de Mensagens

---
+++ Assets/Scripts/MessageBox.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MessageBox : MonoBehaviour
+{
+    private const float SIZE_PER_LINE = 40;
+
+    [SerializeField] private RectTransform frame;
+    [SerializeField] private TMP_Text content;
+
+    public void SetContent(Message message)
+    {
+        content.text = message.content;
+    }
+    public void StartAnimation()
+    {
+        StartCoroutine(ReceiveMessageAnimation());
+    }
+    private float CalculateHeight()
+    {
+        content.ForceMeshUpdate();
+        return Mathf.Clamp(content.textInfo.lineCount * SIZE_PER_LINE, 70, 600);
+    }
+    private IEnumerator ReceiveMessageAnimation()
+    {
+        var messageSpace = FindObjectOfType<MessageSpace>().gameObject.GetComponent<RectTransform>();
+        float target = CalculateHeight();
+        float speed = target / 0.25f;
+        frame.sizeDelta = new Vector2(frame.sizeDelta.x, 0);
+        while (frame.sizeDelta.y < target)
+        {
+            frame.sizeDelta += Vector2.up * Time.deltaTime * speed;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(messageSpace);
+            yield return null;
+        }
+        yield return null;
+    }
+}
+public struct Message
+{
+    public string content;
+    public Message(string content)
+    {
+        this.content = content;
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Animation());
+        FindObjectOfType<MessageSpace>().ReceiveAnswer += ReceiveTest;
+    }
+    private IEnumerator Animation()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            FindObjectOfType<MessageSpace>().NewMessage(new Message("Hello, World!\nHohohoho\nheheheh\ncatchau"));
+            
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    public void ReceiveTest(int index)
+    {
+        print("RECEBI A RESPOSTA::" + index);
+    }
+}
